02690dfd3a08185cc1ac1851c690553d3eab1c8e
Mariana1996 <mariana1996.mph@gmail.com>
2022-06-01 17:14:58 -0500

    Create error  ConfigurationChanges

--- app/src/main/java/com/example/formviewmodelusekotlin/MainActivity.kt
+++ app/src/main/java/com/example/formviewmodelusekotlin/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.formviewmodelusekotlin
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.formviewmodelusekotlin.databinding.ActivityMainBinding
@@ -7,9 +8,22 @@ import com.example.formviewmodelusekotlin.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    //@SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        with(binding){
+            txtResume.text= "No hay datos disponibles"
+            btnSubmit.setOnClickListener{
+                val actualValue= txtResume.text
+                txtResume.text= "Nombre: " +edtName.text  +"\n"+
+                        "Apellidos: "+edtLastName.text +"\n"+
+                        "Telefono: "+ edtPhone.text +"\n"+
+                        "Cedula: "+ edtDNI.text +"\n"+
+                         "Edad: "+edtAge.text  
+            }
+
+        }
     }
 }
