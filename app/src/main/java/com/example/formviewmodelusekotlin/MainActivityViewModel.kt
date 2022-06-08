e557ad1e014761a8f8a510c186e1c8cb01a0604b
Mariana1996 <mariana1996.mph@gmail.com>
2022-06-07 21:36:00 -0500

    Correct implementation liveData (good practices)

--- app/src/main/java/com/example/formviewmodelusekotlin/MainActivityViewModel.kt
+++ app/src/main/java/com/example/formviewmodelusekotlin/MainActivityViewModel.kt
@@ -1,20 +1,38 @@
 package com.example.formviewmodelusekotlin
 
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel : ViewModel() {
 
-    internal var resume = MutableLiveData<String>()
+    private var _resume = MutableLiveData<String>()
+
+    /*
+    * Por fuera es accedido (solo lectura), y solo puede ser cambiado
+    * por el viewModel
+    */
+
+    /*
+    * Crear viewModel con LiveData (mini encapsulación, las clases que lo
+    * suscriben no pueden modificar)
+    */
+    internal  val resume: LiveData<String>
+        get()=_resume
+
+    /*
+    * Por buenas practicas hay que encapsular la info
+    * internal var resume = MutableLiveData<String>()
+    */
 
     init{
-        resume.value= "No hay datos disponibles"
+        _resume.value= "No hay datos disponibles"
     }
 
 
     fun getUpdatedResume(name: String, lastname: String, phone:String, dni:String,age:String){
-        resume.value="Nombre: $name" +"\n"+
+        _resume.value="Nombre: $name" +"\n"+
                 "Apellidos: $lastname" +"\n"+
                 "Telefono: $phone "+"\n"+
                 "Cedula: $dni "+"\n"+
